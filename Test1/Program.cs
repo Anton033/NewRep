84949ea975c59e833333b4adc24285e5761edd10
Anton Sokolovsky <reklam910@gmail.com>
2023-02-21 21:19:40 +0100

    First task from HomeTask2

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 
-class Hello
+internal class Program
 {
     static void Main(string[] args)
     {
+        Task1();
+        Task2();
+    }
+
+    public static void Task1()
+    {
         string name = Console.ReadLine();
         Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello " + name);
+        Console.WriteLine("Helloy " + name);
     }
+
+    public static void Task2()
+    {
+        int operand1 = 5;
+        int operand2 = 4;
+        string action = Console.ReadLine();
+
+        switch (action)
+        {
+            case "*":
+                Console.WriteLine($"The result is: {operand1} * {operand2} = {operand1* operand2}");
+                //Console.WriteLine($"{operand1} * {operand2} = {operand1 * operand2}");
+                break;
+
+            case "/":
+                Console.WriteLine($"The result is: {operand1} / {operand2} = " + (operand1 / operand2));
+                break;
+
+            case "-":
+                Console.WriteLine($"The result is: {operand1} - {operand2} = " + (operand1 - operand2));
+                break;
+
+            case "+":
+                Console.WriteLine($"The result is: {operand1} + {operand2} = " + (operand1 + operand2));
+                break;
+
+        }
+
+
+    
+
+
+    }
+
 }
