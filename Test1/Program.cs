b52a4324c5a5f42b9883e4d6a2bcc2d4f944f597
Anton Sokolovsky <reklam910@gmail.com>
2023-02-22 17:25:48 +0100

    uncommented Tasks1-4

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -4,10 +4,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        //Task1();
-        //Task2();
-        //Task3();
-        //Task4();
+        Task1();
+        Task2();
+        Task3();
+        Task4();
         Task5();
     }
 
