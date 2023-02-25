289bed24d5b730d90ca938812dcf12dbd60e8cea
Anton Sokolovsky <reklam910@gmail.com>
2023-02-25 14:03:47 +0100

    Added HomeTask3

---
+++ Test1/HomeTask3.cs
@@ -0,0 +1,13 @@
+﻿
+
+    internal class HomeTask3
+    {
+        public static void Task1()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helloy " + name);
+        }
+    }
+
+
