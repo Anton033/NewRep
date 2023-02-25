535514ac6d5f8b2ff1468c1e4aee443a8a5925d6
Anton Sokolovsky <reklam910@gmail.com>
2023-02-25 13:34:37 +0100

    Small improves in accordance with style guides

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -21,15 +21,15 @@ internal class Program
 
 
 
-    /* Напишите программу - консольный калькулятор.
-    Создайте две переменные с именами operand1 и operand2.
-    Задайте переменным некоторые произвольные значения.
-    Предложите пользователю ввести знак арифметической операции.
-    Примите значение, введенное пользователем, и поместите его в строковую переменную sign.
-    Для организации выбора алгоритма вычислительного процесса, используйте переключатель switch.
-    Выведите на экран результат выполнения арифметической операции.
-    **В случае использования операции деления, организуйте проверку попытки деления на ноль.
-    И если таковая имеется, то отмените выполнение арифметической операции и уведомите об ошибке пользователя. */
+    //Напишите программу - консольный калькулятор.
+    //Создайте две переменные с именами operand1 и operand2.
+    //Задайте переменным некоторые произвольные значения.
+    //Предложите пользователю ввести знак арифметической операции.
+    //Примите значение, введенное пользователем, и поместите его в строковую переменную sign.
+    //Для организации выбора алгоритма вычислительного процесса, используйте переключатель switch.
+    //Выведите на экран результат выполнения арифметической операции.
+    //**В случае использования операции деления, организуйте проверку попытки деления на ноль.
+    //И если таковая имеется, то отмените выполнение арифметической операции и уведомите об ошибке пользователя.
 
     public static void Task2()
     {
@@ -47,27 +47,22 @@ internal class Program
             case "*":
                 Console.WriteLine($"The result is: {operand1} * {operand2} = {operand1 * operand2}");
                 break;
-
             case "/":
                 if (operand2 == 0)
                 {
                     Console.WriteLine("ERROR");
                 }
-
                 else
                 {
                     Console.WriteLine($"The result is: {operand1} / {operand2} = " + (operand1 / operand2));
                 }
                 break;
-
             case "-":
                 Console.WriteLine($"The result is: {operand1} - {operand2} = " + (operand1 - operand2));
                 break;
-
             case "+":
                 Console.WriteLine($"The result is: {operand1} + {operand2} = " + (operand1 + operand2));
                 break;
-
             default:
                 Console.WriteLine("Incorrect sign was entered");
                 break;
@@ -77,34 +72,30 @@ internal class Program
 
 
 
-    /*Напишите программу определения, попадает ли указанное пользователем число от 0 до 100 в числовой промежуток[0 - 14][15 - 35][36 - 50][50 - 100].
-    Если да, то укажите, в какой именно промежуток.
-    Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков, то выводится соответствующее сообщение. */
+    //Напишите программу определения, попадает ли указанное пользователем число от 0 до 100 в числовой промежуток[0 - 14][15 - 35][36 - 50][50 - 100].
+    //Если да, то укажите, в какой именно промежуток.
+    //Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков, то выводится соответствующее сообщение.
 
     public static void Task3()
     {
         Console.WriteLine("Enter a value from 0 to 100");
-        int a = int.Parse(Console.ReadLine());
+        int number = int.Parse(Console.ReadLine());
 
 
-        switch (a)
+        switch (number)
         {
             case >= 0 and <= 14:
-                Console.WriteLine($"{a} somhere between 0 and 14");
+                Console.WriteLine($"{number} somhere between 0 and 14");
                 break;
-
             case >= 15 and <= 35:
-                Console.WriteLine($"{a} somhere between 15 and 35");
+                Console.WriteLine($"{number} somhere between 15 and 35");
                 break;
-
             case >= 36 and <= 50:
-                Console.WriteLine($"{a} somhere between 36 and 50");
+                Console.WriteLine($"{number} somhere between 36 and 50");
                 break;
-
             case >= 50 and <= 100:
-                Console.WriteLine($"{a} somhere between 50 and 100");
+                Console.WriteLine($"{number} somhere between 50 and 100");
                 break;
-
             default:
                 Console.WriteLine("Out of value range");
                 break;
@@ -113,58 +104,57 @@ internal class Program
     }
 
 
-    /*Напишите программу русско-английский переводчик.
-    Программа знает 10 слов о погоде.
-    Требуется, чтобы пользователь вводил слово на русском языке, а программа давала ему перевод этого слова на английском языке.
-    Если пользователь ввел слово, для которого отсутствует перевод, то следует вывести сообщение, что такого слова нет.*/
+    //Напишите программу русско-английский переводчик.
+    //Программа знает 10 слов о погоде.
+    //Требуется, чтобы пользователь вводил слово на русском языке, а программа давала ему перевод этого слова на английском языке.
+    //Если пользователь ввел слово, для которого отсутствует перевод, то следует вывести сообщение, что такого слова нет.
 
     public static void Task4()
     {
         Console.WriteLine("Enter ru word");
 
-        string a = Console.ReadLine();
+        string word = Console.ReadLine();
 
-
-        switch (a)
+        switch (word)
         {
             case "дождь":
-                Console.WriteLine($"{a} is rain in english");
+                Console.WriteLine($"{word} is rain in english");
                 break;
 
             case "туман":
-                Console.WriteLine($"{a} is fog in english");
+                Console.WriteLine($"{word} is fog in english");
                 break;
 
             case "ветер":
-                Console.WriteLine($"{a} is wind in english");
+                Console.WriteLine($"{word} is wind in english");
                 break;
 
             case "солнечно":
-                Console.WriteLine($"{a} is sunny in english");
+                Console.WriteLine($"{word} is sunny in english");
                 break;
 
             case "пасмурно":
-                Console.WriteLine($"{a} is cloudly in english");
+                Console.WriteLine($"{word} is cloudly in english");
                 break;
 
             case "торнадо":
-                Console.WriteLine($"{a} is tornado in english");
+                Console.WriteLine($"{word} is tornado in english");
                 break;
 
             case "закат":
-                Console.WriteLine($"{a} is sunset in english");
+                Console.WriteLine($"{word} is sunset in english");
                 break;
 
             case "восход":
-                Console.WriteLine($"{a} is sunrise in english");
+                Console.WriteLine($"{word} is sunrise in english");
                 break;
 
             case "влажность":
-                Console.WriteLine($"{a} is humidity in english");
+                Console.WriteLine($"{word} is humidity in english");
                 break;
 
             case "давление":
-                Console.WriteLine($"{a} is pressure in english");
+                Console.WriteLine($"{word} is pressure in english");
                 break;
 
             default:
@@ -177,22 +167,21 @@ internal class Program
     }
 
 
-    /*...Напишите программу, которая будет выполнять проверку чисел на четность.
-    Предложите два варианта решения поставленной задачи.*/
+    //Напишите программу, которая будет выполнять проверку чисел на четность.
+    //Предложите два варианта решения поставленной задачи.
 
 
     // Первый вариант
     public static void Task5()
     {
         Console.WriteLine("Enter a number:");
-        int a = int.Parse(Console.ReadLine());
-        int b = 2;
+        int nubmer = int.Parse(Console.ReadLine());
+        int divider = 2;
 
-        if (a % b == 0)
+        if (nubmer % divider == 0)
         {
             Console.WriteLine("Entered number is even");
         }
-
         else
         {
             Console.WriteLine("Entered number is not even");
@@ -204,16 +193,14 @@ internal class Program
     public static void Task6()
     {
         Console.WriteLine("Enter a number:");
-        int a = int.Parse(Console.ReadLine());
+        int nubmer = int.Parse(Console.ReadLine());
         int remainder;
-        int action = Math.DivRem(a, 2, out remainder);
-
+        int action = Math.DivRem(nubmer, 2, out remainder);
 
         if (remainder == 0)
         {
             Console.WriteLine("Entered number is even");
         }
-
         else
         {
             Console.WriteLine("Entered number is not even");
