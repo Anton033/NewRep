3b2da48dffd56d8621ae5f13dd6be7687386416f
Anton Sokolovsky <reklam910@gmail.com>
2023-03-05 16:34:54 +0100

    added new file Classes.cs

---
+++ Test1/Classes/Phone.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test1.Classes
+{
+	internal class Phone
+	{
+		public string nubmer;
+		public string model;
+		public double weight;
+
+		public Phone(string nubmer, string model, double weight)
+		{
+			this.nubmer = nubmer;
+			this.model = model;
+			this.weight = weight;
+		}
+
+		public Phone(string nubmer, string model) : this(nubmer, model, 0)
+		{
+
+		}
+
+		public Phone()
+		{
+
+		}
+
+		public void recieveCall(string CallerName)
+		{
+			Console.WriteLine($"Звонит {CallerName}");
+		}
+
+		public void recieveCall(string CallerName, string CallerNum)
+		{
+			Console.WriteLine($"Звонит {CallerName}, {CallerNum}");
+		}
+
+		public void sendMessage(params string[] phoneNumbers)
+		{
+			Console.WriteLine("Отправляю сообщение на номера:");
+			foreach (string nubmer in phoneNumbers)
+			{
+				Console.WriteLine(nubmer);
+			}
+		}
+
+		public string GetNubmer()
+		{
+			return nubmer;
+		}
+
+		public void Getinfo()
+		{
+			Console.WriteLine($"Number: {nubmer}, Model: {model}, Weight: {weight}");
+		}
+	}
+
+}
