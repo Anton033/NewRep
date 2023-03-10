65d17b60e5298af562019e1beb4219de32662399
Anton Sokolovsky <reklam910@gmail.com>
2023-03-10 13:38:40 +0100

    Task 2 completed

--- Test1/Classes/Clinic.cs
+++ Test1/Classes/Clinic.cs
@@ -1,11 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+//2.Создать программу для имитации работы клиники.
+//Пусть в клинике будет три врача: хирург, терапевт и дантист.
+//Каждый врач имеет метод «лечить», но каждый врач лечит по-своему.Так же предусмотреть класс «Пациент» и класс «План лечения».
+//Создать объект класса «Пациент» и добавить пациенту план лечения.Так же создать метод, который будет назначать врача пациенту согласно  плану лечения.  
+//Если план лечения имеет код 1 – назначить хирурга и выполнить метод лечить.  
+//Если план лечения имеет код 2 – назначить дантиста и выполнить метод лечить.  
+//Если план лечения имеет любой другой код – назначить терапевта и выполнить метод лечить.
+
 namespace Test1.Classes
 {
-	public class Clinic
+	public class Doc
 	{
-		public Clinic()
+		public virtual void Heal()
 		{
+			Console.WriteLine("");
 		}
 	}
+
+	public class Surger : Doc
+	{
+		public override void Heal()
+		{
+			Console.WriteLine("The surgeon is started by the Surger");
+		}
+	}
+
+	public class Therapist : Doc
+	{
+		public override void Heal()
+		{
+			Console.WriteLine("The therapy is started by the Therapist");
+		}
+	}
+
+	public class Dentist : Doc
+	{
+		public override void Heal()
+		{
+			Console.WriteLine("The teeth restore is started by the Dentist");
+		}
+	}
+
+	public class TreatmentPlan
+	{
+		public int code;
+
+		public TreatmentPlan(int code)
+		{
+			this.code = code;
+		}
+
+		public Doc Plan()
+		{
+			if (code == 1)
+			{
+				Console.WriteLine($"Your code nubmer is: {code}");
+				return new Surger();
+			}
+			else if (code == 2)
+			{
+				Console.WriteLine($"Your code nubmer is: {code}");
+				return new Therapist();
+			}
+			else
+			{
+				Console.WriteLine($"Your code nubmer is: {code}");
+				return new Dentist();
+			}
+		}
+	}
+
+	public class Patient : TreatmentPlan
+	{
+		public string patientName;
+
+		public Patient(int code, string patientName) : base(code)
+		{
+			this.patientName = patientName;
+		}
+	}
+
 }
 
--- Test1/Program.cs
+++ Test1/Program.cs
@@ -12,68 +12,81 @@ internal class Program
 {
 	static void Main()
 	{
-			//Task1
-			Phone phone1 = new Phone("+48123123123", "iPhone 13", 154);
-			Phone phone2 = new Phone("+48124124124", "iPhone 6");
-			Phone phone3 = new Phone();
+		////Task1
+		//Phone phone1 = new Phone("+48123123123", "iPhone 13", 154);
+		//Phone phone2 = new Phone("+48124124124", "iPhone 6");
+		//Phone phone3 = new Phone();
 
-			Console.WriteLine("\nPhone 1:");
-			phone1.Getinfo();
-			phone1.recieveCall("Jhonny");
-			Console.WriteLine($"\nPhone number {phone1.GetNubmer()}");
-			phone1.sendMessage("+48123123123", "+48124124124", "+48125125125");
+		//Console.WriteLine("\nPhone 1:");
+		//phone1.Getinfo();
+		//phone1.recieveCall("Jhonny");
+		//Console.WriteLine($"\nPhone number {phone1.GetNubmer()}");
+		//phone1.sendMessage("+48123123123", "+48124124124", "+48125125125");
 
-			Console.WriteLine("\nPhone 2:");
-			phone2.Getinfo();
-			phone1.recieveCall("Jhonny", "+48124124124");
+		//Console.WriteLine("\nPhone 2:");
+		//phone2.Getinfo();
+		//phone1.recieveCall("Jhonny", "+48124124124");
 
-			Console.WriteLine("\nPhone 3:");
-			phone3.Getinfo();
-
-
-			//Task2
-			CreditCard card1 = new CreditCard("4444-3333-2222-1111-0000", 1000);
-			CreditCard card2 = new CreditCard("5555-0000-2222-1111-0000", 100);
-			CreditCard card3 = new CreditCard("0000-1111-2222-1111-0000", 13000);
-
-			Console.WriteLine("\nCard1:");
-			card1.getInfo();
-
-			Console.WriteLine("\nCard2:");
-			card2.getInfo();
-
-			Console.WriteLine("\nCard3:");
-			card3.getInfo();
-
-			card1.addMoney(10);
-
-			card2.addMoney(200);
-
-			card3.moneyWithdrawal(10000);
-
-			Console.WriteLine("\nCard1 after operation with balance:");
-			card1.getInfo();
-
-			Console.WriteLine("\nCard1 after operation with balance:");
-			card2.getInfo();
-
-			Console.WriteLine("\nCard1 after operation with balance:");
-			card3.getInfo();
+		//Console.WriteLine("\nPhone 3:");
+		//phone3.Getinfo();
 
 
-			//Task3
-			CashMachine atm1 = new CashMachine(100, 20, 1);
-			atm1.getInfo();
+		////Task2
+		//CreditCard card1 = new CreditCard("4444-3333-2222-1111-0000", 1000);
+		//CreditCard card2 = new CreditCard("5555-0000-2222-1111-0000", 100);
+		//CreditCard card3 = new CreditCard("0000-1111-2222-1111-0000", 13000);
 
-			Console.WriteLine("\nAfter adding money: ");
-			atm1.addMoney(5, 2, 3);
+		//Console.WriteLine("\nCard1:");
+		//card1.getInfo();
 
-			atm1.getInfo();
+		//Console.WriteLine("\nCard2:");
+		//card2.getInfo();
 
-			Console.WriteLine("\nAfter withdrawing money: ");
+		//Console.WriteLine("\nCard3:");
+		//card3.getInfo();
 
-			atm1.withdrawMoney(1220);
+		//card1.addMoney(10);
 
-			atm1.getInfo();
+		//card2.addMoney(200);
+
+		//card3.moneyWithdrawal(10000);
+
+		//Console.WriteLine("\nCard1 after operation with balance:");
+		//card1.getInfo();
+
+		//Console.WriteLine("\nCard1 after operation with balance:");
+		//card2.getInfo();
+
+		//Console.WriteLine("\nCard1 after operation with balance:");
+		//card3.getInfo();
+
+
+		////Task3
+		//CashMachine atm1 = new CashMachine(100, 20, 1);
+		//atm1.getInfo();
+
+		//Console.WriteLine("\nAfter adding money: ");
+		//atm1.addMoney(5, 2, 3);
+
+		//atm1.getInfo();
+
+		//Console.WriteLine("\nAfter withdrawing money: ");
+
+		//atm1.withdrawMoney(1220);
+
+		//atm1.getInfo();
+
+		Patient p1 = new Patient(3, "Tom");
+		Patient p2 = new Patient(1, "Jack");
+		Patient p3 = new Patient(2, "Vasily");
+
+		Doc doc1 = p1.Plan();
+		doc1.Heal();
+
+		Doc doc2 = p2.Plan();
+		doc2.Heal();
+
+		Doc doc3 = p3.Plan();
+		doc3.Heal();
 	}
 }
