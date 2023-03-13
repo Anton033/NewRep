1e2d1d15775a7fdd11b4c514ab475698dbb3d81e
Anton Sokolovsky <reklam910@gmail.com>
2023-03-14 00:18:04 +0100

    Add new .cs files

---
+++ Test1/Classes/Buss.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//namespace Test1.Classes
+//{
+//	public class Buss
+//	{
+//		public string Destination { get; set; }
+//		public int Number { get; set; }
+//		public DateTime DepartureTime { get; set; }
+//		public int NumberOfSeats { get; set; }
+
+//		public Buss()
+//		{
+//		}
+
+//		public string getDestination()
+//		{
+//			return destination;
+//		}
+
+//		public void setDestination( string destination)
+//		{
+//			this.destination = destination;
+//		}
+//	}
+//}
+
