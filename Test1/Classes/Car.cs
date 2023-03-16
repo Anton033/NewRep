8691e1fca5f2487b3a503b21c95f22b661a57a9d
Anton Sokolovsky <reklam910@gmail.com>
2023-03-16 22:38:29 +0100

    Added Cars.cs and implementation of Task4

---
+++ Test1/Classes/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace Test1.Classes
+{
+	public abstract class Auto
+	{
+		public string Brand { get; set; }
+		public int Number { get; set; }
+		public double Weight  { get; set; }
+		public int Speed { get; set; }
+
+		public abstract string GetCarInfo();
+		public abstract double loadСapacity();
+
+	}
+
+	public class PassengerCar : Auto
+	{
+		public override string GetCarInfo()
+		{
+			return "Passenger Car";
+		}
+
+		public override double loadСapacity()
+		{
+			return Weight / 3;
+		}
+	}
+
+	public class FreightCar : Auto
+	{
+		public string Trailer { get; set; }
+		public override string GetCarInfo()
+		{
+			return "Fright Car";
+		}
+
+		public override double loadСapacity()
+		{
+			if (Trailer != null)
+			{
+				return Weight * 2;
+			}
+			else
+			{
+				return Weight;
+			}
+		}
+	}
+
+	public class Motorcycle : Auto
+	{
+		public string PassangerSeat { get; set; }
+
+		public override string GetCarInfo()
+		{
+			return "Motorcycle";
+		}
+
+		public override double loadСapacity()
+		{
+			if (PassangerSeat != null)
+			{
+				return Weight / 5;
+			}
+			else
+			{
+				return Weight = 0;
+			}
+		}
+	}
+
+	public class SUV : PassengerCar
+	{
+	}
+
+	public class Sedan : PassengerCar
+	{
+	}
+
+	public class Truck : FreightCar
+	{
+	}
+
+	public class Lorry : FreightCar
+	{
+	}
+
+	public class SportBike : Motorcycle
+	{
+	}
+
+	public class Chopper : Motorcycle
+	{
+	}
+
+
+
+
+}
+
