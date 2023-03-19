d52a78893a45ea8087016427d5d7faa828be8021
Anton Sokolovsky <reklam910@gmail.com>
2023-03-19 21:53:26 +0100

    Task 0 - first point

---
+++ Test1/HW 9/Task0/Task0.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Test1.Task0.HW9
+{
+	internal class Task0
+	{
+		static void Main()
+		{
+			ArrayList list = new ArrayList();
+
+			try
+			{
+				object s = list[18];
+			}
+			catch (ArgumentOutOfRangeException ex)
+			{
+				Console.WriteLine(ex.GetType().Name);
+				Console.WriteLine($"Message: {ex.Message}");
+			}
+		}
+	}
+}
+
