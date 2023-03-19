8e82bf20ba861eec094418cc2d4a552398c6fb1b
Anton Sokolovsky <reklam910@gmail.com>
2023-03-19 22:33:48 +0100

    adding files

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -5,11 +5,15 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test1.HW9;
+namespace Test1.HW9.Task0;
 
 using System.Collections;
 using System.Text.RegularExpressions;
 
 internal class Program
 {
+	static void Main()
+	{
+		Task0.ZeroTask();
+	}
 }
