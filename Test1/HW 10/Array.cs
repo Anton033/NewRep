4becd9076704d7d4b162fff97501d630cdc0705a
Anton Sokolovsky <reklam910@gmail.com>
2023-03-27 22:53:16 +0200

    First task Generic Array class

---
+++ Test1/HW 10/Array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Test1.HW10
+{
+	public class Generic<T>
+	{
+		public T[] data;
+
+		public Generic()
+		{
+			data = new T[0];
+		}
+
+		public int Length
+		{
+			get { return data.Length; }
+		}
+
+		public void AddToArray(T item)
+		{
+			T[] newData = new T[data.Length + 1];
+			data.CopyTo(newData, 0);
+			newData[data.Length] = item;
+			data = newData;
+		}
+
+		public void RemoveFromArray(int index)
+		{
+			T[] newData = new T[data.Length - 1];
+			for (int i = 0; i < index; i++)
+			{
+				newData[i] = data[i];
+			}
+			for (int i = index + 1; i < data.Length; i++)
+			{
+				newData[i - 1] = data[i];
+			}
+			data = newData;
+		}
+
+		public T GetArrayItem(int index)
+		{
+			return data[index];
+		}
+	}
+}
+
